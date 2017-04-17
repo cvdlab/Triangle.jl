65b27d81e64fd25e500072f16f4bbbba03011ebc
furio <francesco@furio.me>
2017-04-17 17:52:53 +0200

    c type

--- src/TRIANGLE.jl
+++ src/TRIANGLE.jl
@@ -10,4 +10,36 @@ end
 
 print("Started")
 
+# REAL = double = Cdouble
+type triangulateio
+  pointlist::Array{Cdouble}
+  pointattributelist::Array{Cdouble}
+  pointmarkerlist::Array{Cint}                                         /* In / out */
+  numberofpoints::Cint                                           /* In / out */
+  numberofpointattributes::Cint                                   /* In / out */
+
+  trianglelist::Array{Cint}                                           /* In / out */
+  triangleattributelist::Array{Cdouble}                                   /* In / out *
+  trianglearealist::Array{Cdouble}                                        /* In only */
+  neighborlist::Array{Cint}                                          /* Out only */
+  numberoftriangles::Cint                                         /* In / out */
+  numberofcorners::Cint                                          /* In / out */
+  numberoftriangleattributes::Cint                                /* In / out */
+
+  segmentlist::Array{Cint}                                                /* In / out */
+  segmentmarkerlist::Array{Cint}                                          /* In / out */
+  numberofsegments::Cint                                          /* In / out */
+
+  holelist::Array{Cdouble}                        /* In / pointer to array copied out */
+  numberofholes::Cint                                      /* In / copied out */
+
+  regionlist::Array{Cdouble}                      /* In / pointer to array copied out */
+  numberofregions::Cint                                    /* In / copied out */
+
+  edgelist::Array{Cint}                                                   /* Out only */
+  edgemarkerlist::Array{Cint}              /* Not used with Voronoi diagram; out only */
+  normlist::Array{Cdouble}               /* Used only with Voronoi diagram; out only */
+  numberofedges::Cint                                             /* Out only *
+end
+
 end
