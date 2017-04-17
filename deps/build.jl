679f48f42e34a08d20225316521a9f8da312e76c
furio <francesco@furio.me>
2017-04-17 16:47:14 +0200

    Managed build deps for *nix

--- deps/build.jl
+++ deps/build.jl
@@ -1,23 +1,40 @@
 using BinDeps
 
+isfile("deps.jl") && rm("deps.jl")
+
 @BinDeps.setup
+    deps = [libnix = library_dependency("libtriangle", aliases = ["libtriangle.so","libtriangle.dylib"], runtime = false, os = :Unix),
+            libwin = library_dependency("libtriangle", aliases = ["libtriangle.dll"], runtime = false, os = :Windows)]
+    rootdir = BinDeps.depsdir(libnix)
+    srcdir = joinpath(rootdir, "src")
+    prefix = joinpath(rootdir, "usr")
+    libdir = joinpath(prefix, "lib")
+    headerdir = joinpath(prefix, "include")
 
-libtriangle = library_dependency("libtriangle")
+    if is_windows()
+        libfile = joinpath(libdir, "libtriangle.dll")
+    else 
+        libname = "libtriangle.so"
+        if is_apple()
+            libname = "libtriangle.dylib"
+        end
+        libfile = joinpath(libdir, libname)
+        provides(BinDeps.BuildProcess, (@build_steps begin
+                    FileRule(libfile, @build_steps begin
+                        BinDeps.ChangeDirectory(srcdir)
+                        `make clean`
+                        `make`
+                        `cp libtriangle.so $libfile`
+                        `cp triangle.h $headerdir/`
+                        `cp tricall.h $headerdir/`
+                        `cp commondefine.h $headerdir/`
+                        `make clean`
+                    end)
+                end), libnix)
+    end
 
-# provides(Binaries, URI("https://cache.julialang.org/https://bintray.com/artifact/download/tkelman/generic/gumbo.7z"),
-#         libgumbo, unpacked_dir="usr$(Sys.WORD_SIZE)/bin", os = :Windows)
-
-_prefix = joinpath(BinDeps.depsdir(libtriangle),"usr")
-_libdir = joinpath(_prefix, "lib")
-
-@static if is_linux()
-    provides(BuildProcess,
-         Autotools(libtarget="libtriangle.so"),
-         libtriangle, os = :Unix)
+if is_windows()
+    @BinDeps.install Dict(:libwin => :_jl_libtriangle)
+else
+    @BinDeps.install Dict(:libnix => :_jl_libtriangle)
 end
-
-@static if is_windows()
-
-end        
-
-@BinDeps.install Dict(:libtriangle => :libtriangle)
--- deps/src/commondefine.h
+++ deps/src/commondefine.h
@@ -1,9 +1,6 @@
 #ifndef COMMONDEFINE_H
     #define REAL double
     #define _NDIM 2
-    #ifdef _MSC_VER
-        #define CPU86 1
-    #endif
 
     #define TRIANGULATEIO_NAME "triangulateio"
     #define CDT_ONLY 1
@@ -12,14 +9,15 @@
     #define NO_TIMER 1
     #define ANSI_DECLARATORS 1
 	#define VOID int
-    #define ANSILOCALDECL 
-	
+    
     #ifdef _MSC_VER // Visual Studio specific macro
         #define DLLEXPORT __declspec(dllexport)
         #define DLLLOCAL
+        #define ANSILOCALDECL 
     #else 
         #define DLLEXPORT __attribute__ ((visibility("default")))
         #define DLLLOCAL __attribute__ ((visibility("hidden")))
+        #define ANSILOCALDECL 
     #endif 
 
 	#define COMMONDEFINE_H
