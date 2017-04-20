7ca2dfd2354712b53acd7d509eb47216cb6e35a8
furio <francesco@furio.me>
2017-04-20 15:47:40 +0200

    Build steps

--- deps/build.jl
+++ deps/build.jl
@@ -6,6 +6,9 @@ isfile("deps.jl") && rm("deps.jl")
     deps = [libnix = library_dependency("libtriangle", aliases = ["libtriangle.so","libtriangle.dylib"], runtime = false, os = :Unix),
             libwin = library_dependency("libtriangle", aliases = ["libtriangle.dll"], runtime = false, os = :Windows)]
     rootdir = BinDeps.depsdir(libnix)
+    if is_windows()
+        rootdir = BinDeps.depsdir(libwin)
+    end
     srcdir = joinpath(rootdir, "src")
     prefix = joinpath(rootdir, "usr")
     libdir = joinpath(prefix, "lib")
@@ -13,6 +16,18 @@ isfile("deps.jl") && rm("deps.jl")
 
     if is_windows()
         libfile = joinpath(libdir, "libtriangle.dll")
+        userdit = ENV["USERPROFILE"]
+        vcpython="AppData\\Local\\Programs\\Common\\Microsoft\\Visual\ C++\ for\ Python\\9.0\\vcvarsall.bat"
+        provides(BinDeps.SimpleBuild, (@build_steps begin
+                 BinDeps.run(@build_steps begin
+                    ChangeDirectory(srcdir)
+                    `$userdit\\$vcpython & nmake -f makefile.win clean & nmake -f makefile.win`
+                    `cmd /c copy libtriangle.dll $libfile`
+                    `cmd /c copy triangle.h $headerdir`
+                    `cmd /c copy tricall.h $headerdir`
+                    `cmd /c copy commondefine.h $headerdir`
+                    `$userdit\\$vcpython & nmake -f makefile.win clean`
+            end) end), libwin)
     else 
         libname = "libtriangle.so"
         if is_apple()
